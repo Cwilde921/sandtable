38db302dfe8e7fa375d31fe1fb828dd7a3b2ce57
Carson <wilde.utah@gmail.com>
2021-04-19 21:38:59 -0600

    Update shell.py

--- sand_table/src/shell.py
+++ sand_table/src/shell.py
@@ -6,13 +6,18 @@ def loop():
     reader = Reader()
     while True:
         inpt = input("->")
-        inpt = inpt.split('\n')
-        for l in inpt:
-            try:
-                cmd = reader.read_line_thr(l)
-                table.goto(cmd)
-            except:
-                print("An error ocurred. Invalid command")
+        if "exit" in inpt:
+            break
+        elif "set" in inpt:
+            pass
+        else:
+            inpt = inpt.split('\n')
+            for l in inpt:
+                try:
+                    cmd = reader.read_line_thr(l)
+                    table.goto(cmd)
+                except:
+                    print("\nAn error ocurred. Invalid command")
 
 try:
     loop()
