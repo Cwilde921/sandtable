e095bf930d0edbe3dbcee0182614ffdcd3d497c4
Carson <wilde.utah@gmail.com>
2021-04-20 21:43:57 -0600

    Update Table.py

--- sand_table/src/Table.py
+++ sand_table/src/Table.py
@@ -14,7 +14,7 @@ class Table:
         self.sleep_time = config['delay_ms'] / 1000
         self.pos = {'th': 0, 'r': 0}
         self.goal = {'th': 0, 'r': 0}
-        th_full_steps = math.pi/1024
+        # th_full_steps = math.pi/1024
         self.step_motion_th = config['motion']['th'] #{'th': th_full_steps, 'r': (th_full_steps/10)}
         self.step_motion_r = config['motion']['r'] #{'th': 0, 'r': (1/2048)}
         #self.reader = Reader()
@@ -74,6 +74,12 @@ class Table:
             bm = self.best_move()
             self.move(bm)
             #sleep(self.sleep_time)
+            
+    def get_pos(self):
+        return self.pos
+    
+    def set_pos(self, pos):
+        self.pos = pos)
 
     #def execute(self, filename):
     #    self.reader.read_exec_file(filename, self.goto)
