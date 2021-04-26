d7db9f1b54488b2a2597ac03eeb06764377c8211
= <wilde.utah@gmail.com>
2021-04-25 23:22:07 -0600

    fixing issues in main

--- sand_table/src/main.py
+++ sand_table/src/main.py
@@ -1,15 +1,50 @@
 #! /usr/bin/python3
 
 import sys
-from shell import loop
 from Reader import Reader
 from Table import Table
 from config import config
 
 args = sys.argv
 
+reader = Reader()
+table = Table()
+
 if '--shell' in args or '-sh' in args:
-    loop()
+    while True:
+        inpt = input( "{%.2f} {%.2f} -> ".format(table.get_pos()['th'], table.get_pos()['r']) )
+        if "help" in inpt:
+            print("\n\n" +\
+                "\t======== Sand Table Shell Help ========\n" +\
+                "exit:                 Exit program.\n" +\
+                "set:\n" +\
+                "    home:             Set current position to 0, 0.\n" +\
+                "file:\n" +\
+                "    <filename>:       Fun file from patterns directory.\n" +\
+                "<theta> <rho>:        Goto position specified.\n" +\
+                "\t=============== End Help ==============\n\n" 
+            )
+
+        if "exit" in inpt:
+            break
+        elif "set" in inpt:
+            if "home" in inpt:
+                table.set_pos({'th': 0, 'r':0})
+        elif "file" in inpt:
+            args = inpt.split(' ')
+            fname = args[args.index("file")+1]
+            path = config['pattern_dir']
+            if(path[-1] != '/'): path = path + '/'
+            fname = path + fname
+        
+        else:
+            inpt = inpt.split('\n')
+            for l in inpt:
+                try:
+                    cmd = reader.read_line_thr(l)
+                    table.goto(cmd)
+                except:
+                    print("\nAn error ocurred. Invalid command")
     sys.exit()
 
 file = -1
@@ -19,8 +54,41 @@ elif '-f' in args:
     file = args.index("-f")
 
 if file >= 0:
-    file = config['pattern_dir'] + args[file+1]
-    reader = Reader()
-    table = Table()
-    reader.read_exec_file(file, table.goto)
+    path = config['pattern_dir']
+    if(path[-1] != '/'): path = path + '/'
+    fname = path + fname
+    reader.read_exec_file(fname, table.goto)
     sys.exit()
+
+if '--help' in args or '-h' in args:
+    print("\n" +\
+        "================ Help ================\n" +\
+        "--help:   -h:               Show this help\n" +\
+        "--shell:  -sh:              Start sand table shell\n" +\
+        "--file:   -f:   <filename>  Run specified file\n" +\
+        "=============== End Help ==============\n" 
+    )
+
+# def loop(table, reader):
+#     while True:
+#         inpt = input( "{%.2f} {%.2f} -> ".format(table.get_pos()['th'], table.get_pos()['r']) )
+#         if "exit" in inpt:
+#             break
+#         elif "set" in inpt:
+#             if "home" in inpt:
+#                 table.set_pos({'th': 0, 'r':0})
+#         elif "file" in inpt:
+#             args = inpt.split(' ')
+#             fname = args[args.index("file")+1]
+#             path = config['pattern_dir']
+#             if(path[-1] != '/'): path = path + '/'
+#             fname = path + fname
+        
+#         else:
+#             inpt = inpt.split('\n')
+#             for l in inpt:
+#                 try:
+#                     cmd = reader.read_line_thr(l)
+#                     table.goto(cmd)
+#                 except:
+#                     print("\nAn error ocurred. Invalid command")
---
+++ sand_table/src/mytest.py
@@ -0,0 +1,18 @@
+print("\n\n" +\
+    "\t======== Sand Table Shell Help ========\n" +\
+    "exit:                 Exit program.\n" +\
+    "set:\n" +\
+    "    home:             Set current position to 0, 0.\n" +\
+    "file:\n" +\
+    "    <filename>:       Fun file from patterns directory.\n" +\
+    "<theta> <rho>:        Goto position specified.\n" +\
+    "\t=============== End Help ==============\n\n" 
+)
+
+print("\n" +\
+    "================ Help ================\n" +\
+    "--help:   -h:               Show this help\n" +\
+    "--shell:  -sh:              Start sand table shell\n" +\
+    "--file:   -f:   <filename>  Run specified file\n" +\
+    "=============== End Help ==============\n" 
+)
