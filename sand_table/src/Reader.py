1480f2c5840e788ff00c8af9c4aa58579f444d72
= <wilde.utah@gmail.com>
2021-05-02 16:31:38 -0600

    beggining refactor

--- sand_table/src/Reader.py
+++ sand_table/src/Reader.py
@@ -3,118 +3,144 @@ import glob
 import random
 from config import config
 
+
 class Reader:
-    def __init__(self):
-        self.__queue = []
-        self.file_data = {}
+    # def read_exec_file(self, filename, handler, clear_first=False):
+    #     print(filename)
+    #     with open(filename, 'r') as f:
+    #         for line in f:
+    #             handler( read_line_thr(line) )
 
-    def enqueue_back(self, filename):
-        self.__queue.append(filename)
-
-    def enqueue_front(self, filename):
-        self.__queue.insert(0, filename)
-
-    def _dequeue(self):
-        if len(self.__queue) > 0:
-            return self.__queue.pop(0)
-        else:
-            return random.choice(glob.glob(config['pattern_dir']+"*.thr"))
-
-    def get_random_file(self):
-        # os.listdir(config["pattern_dir"])
-        f = random.choice(glob.glob(config['pattern_dir']+"*.thr"))
-        return f
-
-    def run(self, table, do_home=False):
-        while True:
-            f = self._dequeue()
-            self.file_data = {}
-            self.get_file_data(f)
-            #if 'Start Point' in self.file_data:
-            #    if self.file_data['Start Point'] == 'center':
-            #        self.read_exec_file(config['pattern_dir']+'clear/center.thr', handler, clear_first=False)
-            #    else:
-            #        self.read_exec_file(config['pattern_dir']+'clear/perimeter.thr', handler, clear_first=False)
-            if do_home:
-                table.go_home()
-            self.read_exec_file(f, table.goto, clear_first=True)
-            
-
-    def read_exec_file(self, filename, handler, clear_first=False):
-        print(filename)
+    @staticmethod
+    def get_file_data(filename):
+        file_data = {}
         with open(filename, 'r') as f:
             for line in f:
-                handler( self.read_line_thr(line) )
+                l = Reader.read_file_data_line(line)
+                for key in l:
+                    file_data[key] = l[key]
+        return file_data
 
-    def get_file_data(self, filename):
-        with open(filename, 'r') as f:
-            for line in f:
-                line = line.strip()
-                idx = line.find('#')
-                if idx == 0:
-                    line = line[1:]
-                    line = line.strip()
-                    if ':' in line:
-                        line = line.split(':')
-                        for i in range(len(line)):
-                            line[i] = line[i].strip()
-                        if len(line) == 2:
-                            try: 
-                                line[1] = float(line[1])
-                            except ValueError:
-                                pass
-                        self.file_data[line[0]] = line[1]
-
-                
-
-    def read_line_thr(self, line:str, get_data=False):
+    @staticmethod
+    def read_file_data_line(line):
+        res = {}
         line = line.strip()
         idx = line.find('#')
         if idx == 0:
-            #if get_data:
-            #    line = line[1:]
-            #    line = line.strip()
-            #    if ':' in line:
-            #        line = line.split(':')
-            #        for i in range(len(line)):
-            #            line[i] = line[i].strip()
-            #        if len(line) == 2:
-            #            try: 
-            #                line[1] = float(line[1])
-            #            except ValueError:
-            #                pass
-            #        self.file_data[line[0]] = line[1]
-            return {}
+            line = line[1:]
+            line = line.strip()
+            if ':' in line:
+                line = line.split(':')
+                for i in range(len(line)):
+                    line[i] = line[i].strip()
+                if len(line) == 2:
+                    try: 
+                        line[1] = float(line[1])
+                    except ValueError:
+                        pass
+                res[line[0]] = line[1]
+        return res
 
+
+    @staticmethod
+    def validate_file_name(fname, test_append=True):
+        try:
+            with open(fname, 'r'):
+                pass
+            return fname
+        except FileNotFoundError:
+            if not test_append:
+                raise FileNotFoundError
+            path = config['pattern_dir']
+            if path[-1] == '/':
+                path = path[:-1]
+            if fname[0] == '/':
+                fname = fname[1:]
+            if '.' not in fname:
+                fname += '.thr'
+            elif fname[-4:] != '.thr':
+                raise Exception("bad file type")
+            return Reader.validate_file_name(path + '/' + fname, test_append=False)
+
+    @staticmethod
+    def get_random_file(directory_path=None, subfolder="", extension=".thr"):
+        # os.listdir(config["pattern_dir"])
+        if directory_path is None:
+            directory_path = config['pattern_dir']
+        f = random.choice(glob.glob( "{}{}{}*{}".format(directory_path, ('/' if len(subfolder)>0 else ''), subfolder, extension ) ))
+        # f = random.choice(glob.glob( directory_path+"*"+extension ))
+        return f
+
+    @staticmethod
+    def read_line_thr(line:str):
+        line = line.strip()
+        idx = line.find('#')
+        if idx == 0:
+            res = Reader.read_file_data_line(line)
+            res['is_valid'] = False
+            return res
         if idx > 0:
             line = line[:idx]
         line = line.split(' ')
-        if len(line) < 2:
-            return {}
         if len(line) != 2:
-            return {}
+            return {'is_valid': False}
             #raise Exception("===== Error found while reading THR line =====\nWrong number of arguments in line. expected {} got {}".format(2, len(line)))
         return {
                 'th': float(line[0]),
                 'r': float(line[1]),
+                'is_valid': True,
             }
 
-    def print_cmd(self, cmd):
+    @staticmethod
+    def print_cmd(cmd):
         print("th: {}\tr: {}".format(cmd['th'], cmd['r']))
 
+    @staticmethod
+    def open_file(fname, validate_file=True):
+        if validate_file:
+            fname = Reader.validate_file_name(fname)
+        with open(fname, 'r') as f:
+            for line in f:
+                yield line
+
+
+    class FileQueue:
+        def __init__(self):
+            self.__queue = []
+
+        def enqueue(self, filename, pos=None, validate=True):
+            if validate:
+                filename = Reader.validate_file_name(filename)
+            if pos is None:
+                pos = len(self.__queue)
+            self.__queue.insert(pos, filename)
+
+        def pop(self):
+            if len(self.__queue) > 0:
+                return self.__queue.pop(0)
+            else:
+                return None
+
+        def peek(self, pos=0):
+            if len(self.__queue) > pos:
+                return self.__queue[pos]
+            else:
+                return None
+
 if __name__ == '__main__':
-    def func(cmd):
-        if len(cmd) == 0:
-            return
-        r = Reader()
-        r.print_cmd(cmd)
-
-
-    # Reader.read_exec_file("../test_patterns/sandify.thr", func)
-    r = Reader()
-    r.read_exec_file("../test_patterns/sandify(5).thr", func)
-    print(r.file_data)
-    # print(r.get_random_file())
+    fname = "sand_table/src/test.txt"
+    # fname = "patterns/sandify.thr"
+    f = Reader.open_file(fname, validate_file=True)
+    print(next(f))
+    for i in range(10):
+        try:
+            print(next(f))
+        except StopIteration:
+            print("breaking")
+            break
+    print()
+    for cmd in f:
+        print(cmd)
 
 
 
--- sand_table/src/main.py
+++ sand_table/src/main.py
@@ -5,14 +5,9 @@ from Reader import Reader
 from Table import Table
 from config import config
 
-args = sys.argv
-
-reader = Reader()
-table = Table()
-
 def shell_func():
     global table
-    global reader
+    # global reader
     try:
         while True:
             inpt = input( "{:.2f} {:.2f} -> ".format(table.get_pos()['th'], table.get_pos()['r']) )
@@ -52,9 +47,13 @@ def shell_func():
                 fname = path + fname
                 print(fname)
                 try:
-                    reader.read_exec_file(fname, table.goto)
+                    # Reader.read_exec_file(fname, table.goto)
+                    exec_file(fname)
+                    
                 except KeyboardInterrupt:
                     print("stopping {} early".format(fname))
+                except FileNotFoundError:
+                    print("{} not found".format(fname))
             
             else:
                 inpt = inpt.split('\n')
@@ -68,45 +67,59 @@ def shell_func():
         print("\nGoodbye")
     sys.exit()
 
+def exec_file(fname):
+    global table
+    f = Reader.open_file(fname, validate_file=True)
+    for line in f:
+        cmd = Reader.read_line_thr(line)
+        if not cmd['is_valid']:
+            continue
+        table.goto(cmd)
 
 
-if '--go_home' in args or '-gh' in args:
-    table.go_home()
+if __name__ == "__main__":
+    args = sys.argv
 
-if '--shell' in args or '-sh' in args:
-    shell_func()
+    reader = Reader()
+    table = Table()
 
-file = -1
-if '--file' in args:
-    file = args.index("--file")
-elif '-f' in args:
-    file = args.index("-f")
+    if '--help' in args or '-h' in args:
+        print("\n" +\
+            "================ Help ================\n" +\
+            "--help         -h                Show this help\n" +\
+            "--go_home      -gh               Run homing sequence\n" +\
+            "--shell        -sh               Start sand table shell\n" +\
+            "--file         -f   <filename>   Run specified file\n" +\
+            "--run          -r                Run random files continuously\n" +\
+            "=============== End Help ==============\n" 
+        )
+        sys.exit()
 
-if file >= 0:
-    path = config['pattern_dir']
-    if(path[-1] != '/'): path = path + '/'
-    fname = path + args[file+1]
-    reader.read_exec_file(fname, table.goto)
-    sys.exit()
+    if '--go_home' in args or '-gh' in args:
+        table.go_home()
 
-if '--help' in args or '-h' in args:
-    print("\n" +\
-        "================ Help ================\n" +\
-        "--help         -h                Show this help\n" +\
-        "--shell        -sh               Start sand table shell\n" +\
-        "--file         -f   <filename>   Run specified file\n" +\
-        "--run          -r                Run random files continuously\n" +\
-        "--go_home      -gh               Run homing sequence\n" +\
-        "=============== End Help ==============\n" 
-    )
-    sys.exit()
+    if '--shell' in args or '-sh' in args:
+        shell_func()
 
-if '--run' in args or '-r' in args:
-    try:
-        reader.run(table, do_home=True)
-    except KeyboardInterrupt:
-        print("\nGoodbye")
-    sys.exit()
+    file = -1
+    if '--file' in args:
+        file = args.index("--file")
+    elif '-f' in args:
+        file = args.index("-f")
+
+    if file >= 0:
+        exec_file( args[file] )
+        sys.exit()
+
+    if '--run' in args or '-r' in args:
+        try:
+            while True:
+                fname = Reader.get_random_file()
+                exec_file(fname)
+
+        except KeyboardInterrupt:
+            print("\nGoodbye")
+        sys.exit()
 
 # def loop(table, reader):
 #     while True:
