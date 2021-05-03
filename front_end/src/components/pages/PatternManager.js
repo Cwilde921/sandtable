f40798804f62ac1a699116b7de384aa619e4a79d
= <wilde.utah@gmail.com>
2021-05-03 00:28:16 -0600

    web server testing

--- front_end/src/components/pages/PatternManager.js
+++ front_end/src/components/pages/PatternManager.js
@@ -3,21 +3,39 @@ import React from 'react'
 import styles from '../../style/PatternManager.module.css'
 
 export default class PatternManager extends React.Component {
+    constructor(params){
+        super(params)
+        this.res = {
+            file: null,
+        }
+    }
+
+    send_file() {
+        fetch("localhost:5000/api",{
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({'hello': "world"})
+        })//.then((res) => res.json())
+        .then((res) => console.log(res))
+    }
+
     render(){
         return (
             <div className={styles.body}>
                 <h1>Pattern Manager</h1>
                 <p>Make patterns with sandify and upload them here in thr format.</p>
-                <form>
-                    <label htmlFor={"pattern_file"} className={styles.inpt} > Upload Pattern </label>
-                    <input id={"pattern_file"} className={styles.inpt} type={"file"} />
-
-                    <hr/>
-                    {/* <br/> */}
-                    <button className={styles.inpt} onClick={ () => this.props.handler(0) }>Home</button>
+                <form onSubmit={() => {this.send_file()}} >
+                    <label id={"pattern_file_label"} htmlFor={"pattern_file"} className={styles.inpt} > Upload Pattern </label>
+                    <input id={"pattern_file"} className={styles.inpt} type={"file"} onChange={(event)=>{
+                        this.res.file = event.target.files.item(0)
+                        document.getElementById("pattern_file_label").innerHTML = this.res.file.name
+                    }} />
+                    <br/>
                     <input type={"submit"} className={styles.inpt} value={"Submit Pattern"} />
-                    <button className={styles.inpt} onClick={ () => window.location.href="https://sandify.org" } >Go To Sandify</button>
                 </form>
+                <hr/>
+                <button className={styles.inpt} onClick={ () => this.props.handler(0) }>Home</button>
+                <button className={styles.inpt} onClick={ () => window.location.href="https://sandify.org" } >Go To Sandify</button>
             </div>
         )
     }
